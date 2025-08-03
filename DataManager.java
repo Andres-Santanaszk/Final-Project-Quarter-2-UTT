b1156a1c6df19854c16946245945c3ecacb54570
Elsecu <angelsecundino64@hotmail.com>
2025-08-02 19:41:21 -0700

    actualizacion de DataManager

--- DataManager.java
+++ DataManager.java
@@ -1,16 +1,14 @@
 
 public class DataManager {
-   
 
-
-
-
-
-
-
+    public static String[][] usuarios = {
+        {"Andres", "andres123"},
+        {"Angel", "angel123"},
+        {"Julissa", "julissa123"},
+    };
 
     public static int [][] costoEventos(){
-    
+
         int[][] costos = {
             {300, 250, 1200, 500},
             {400, 500, 1500, 350},
@@ -18,5 +16,4 @@ public class DataManager {
         };
         return costos;
     }
-
 }
