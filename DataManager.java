f65b86ced6d696d40d49d03fc0735512bd2c887b
Andrzej-Santanaszk <el_chingon94@hotmail.com>
2025-08-02 17:43:20 -0700

    merge

--- DataManager.java
+++ DataManager.java
@@ -1,10 +1,19 @@
+
 public class DataManager {
+
     public static String[][] usuarios = {
         {"andres", "andres123"},
         {"angel", "angel123"},
         {"julissa", "julissa123"},
     };
 
+    public static int [][] costoEventos(){
     
-
+        int[][] costos = {
+            {120, 304, 300},
+            {150, 500, 200},
+            {40, 432, 400}
+        };
+        return costos;
+    }
 }   
