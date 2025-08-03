d5c576333111b026d16b66dc5c9287319bbf9b1d
Andrzej-Santanaszk <el_chingon94@hotmail.com>
2025-08-02 17:21:57 -0700

    DataManager

--- DataManager.java
+++ DataManager.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DataManager {
 
     public static String[][] usuarios = {
@@ -5,4 +7,14 @@ public class DataManager {
         {"angel", "angel123"},
         {"julissa", "julissa123"},
     };
+
+    public static int [][] costoEventos(){
+    
+        int[][] costos = {
+            {120, 304, 300},
+            {150, 500, 200},
+            {40, 432, 400}
+        };
+        return costos;
+    }
 }   
